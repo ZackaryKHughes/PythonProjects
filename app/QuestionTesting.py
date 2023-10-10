d261da2fcd321dbc44b253d10bd2c4210931b96e
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-10-10 12:12:37 -0500

    Finished Question 2 where I took a input for name and an input for color and return a statement back to the user.

--- app/QuestionTesting.py
+++ app/QuestionTesting.py
@@ -1,6 +1,15 @@
 # Question 1 We Check in a patient named John Smith. He's 20 years old and is a new patient.
 # Fill out variables for patient name, age, and if he is a new or old patient.
 
-name = "John Smith"
-age = 20
-new_patient = True
+# name = "John Smith"
+# age = 20
+# new_patient = True
+
+# Question 2 Ask two questions: person's name and favorite color
+# Then, print a message like "Name likes blue"
+
+name = input("What is your name? ")
+color = input("What is your favorite color? ")
+
+print(name + " likes " + color)
+
