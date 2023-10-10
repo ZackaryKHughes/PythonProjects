59ac5874d4b7d2e0d65db71dca8392af916b7793
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-10-10 11:57:00 -0500

    Filled variables for question 1

--- app/QuestionTesting.py
+++ app/QuestionTesting.py
@@ -1,3 +1,6 @@
+# Question 1 We Check in a patient named John Smith. He's 20 years old and is a new patient.
+# Fill out variables for patient name, age, and if he is a new or old patient.
+
 name = "John Smith"
 age = 20
-newPatient = True
+new_patient = True
