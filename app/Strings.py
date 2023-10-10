3066b4bfe7714079fd8b45d1e032d7919eab1f48
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-10-10 12:51:09 -0500

    Leanred about array and Formated Strings.

---
+++ app/Strings.py
@@ -0,0 +1,12 @@
+course = 'Python for "Beginners"'
+another = course[:]
+print(another)
+
+# Formatted Strings
+
+first = "Zackary"
+last = "Hughes"
+message = first + " [" + last + "] is a coder."
+print(message)
+
+
