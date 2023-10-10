ec5d003db75516bb46effb2e3125c0d35fad3044
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-10-10 12:23:54 -0500

    Learned about type of and conversions.

--- app/input.py
+++ app/input.py
@@ -1,2 +1,7 @@
-name = input("What is your name? ")
-print("Hi " + name)
+# name = input("What is your name? ")
+# print("Hi " + name)
+
+
+birth_year = input("Birth year: ")
+age = 2023 - int(birth_year)
+print(age)
