a509ab1827bb0d935563f9c3edcacfafcaaf4106
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-10-10 12:35:33 -0500

    Finished Question 3 which took a user input for weight in pounds, converted it to kilograms, returned user the kilograms.

--- app/QuestionTesting.py
+++ app/QuestionTesting.py
@@ -8,8 +8,17 @@
 # Question 2 Ask two questions: person's name and favorite color
 # Then, print a message like "Name likes blue"
 
-name = input("What is your name? ")
-color = input("What is your favorite color? ")
+# name = input("What is your name? ")
+# color = input("What is your favorite color? ")
+#
+# print(name + " likes " + color)
 
-print(name + " likes " + color)
+# Question 3 Ask a user their weight (in pounds), convert it to kilograms and print on the terminal
+
+weight = input("What is your weight in pounds? ")
+kilo = int(weight) / 2.20462262
+print(kilo)
+
+
+
 
