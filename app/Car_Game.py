4731e6055cae44f31eec526331946b2dc13a24ff
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-10-11 12:49:59 -0500

    Added more if and elif to check if car is already on or off.

--- app/Car_Game.py
+++ app/Car_Game.py
@@ -1,10 +1,19 @@
 command = ""
+started = False
 while True:
     command = input("> ").lower()
     if command == "start":
+        if started:
+            print("Car is already started")
+        else:
+            started = True
         print("Car Started")
     elif command == "stop":
-        print("Car Stopped")
+        if not started:
+            print("Car is already off")
+        else:
+            started = False
+            print("Car Stopped")
     elif command == "help":
         print("""
 start - start the car
