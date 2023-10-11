05e3948201cfb578710ba4e5af2d3d23036dc871
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-10-11 07:39:30 -0500

    Learned about Logical Operatiors.

---
+++ app/Logical_Operations.py
@@ -0,0 +1,14 @@
+high_income = False
+good_credit = True
+criminal_record = True
+
+if good_credit and not criminal_record:
+    print("Is eligible for loan")
+else:
+    print("I am sorry, not eligible for loan")
+
+# And : both
+# Or: at least one
+# Not: True will change to a False on a variable.
+
+
